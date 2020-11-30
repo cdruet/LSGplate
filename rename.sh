8621bc3573f6c66e153af5d357087b0b6cff362c
git <webdev.stoachup@gmail.com>
2020-11-30 16:45:22 +0100

    Release 1.1.3

--- rename.sh
+++ rename.sh
@@ -3,5 +3,5 @@ set -e
 
 sudo sed -i "s/^.*ap_name: .*\$/ap_name: $1-<nnnn>/g" /etc/comitup.conf
 sudo sed -i "s/^.*plate_name: .*\$/plate_name: $1/g" /etc/lsgplate.conf
-sudo sed -i "s/$HOSTNAME/$1/g" /etc/hosts
-sudo sed -i "s/^.*$HOSTNAME.*\$/$1/g" /etc/hostname
+sudo sed -i "s/^127\.0\.1\.1.*\$/127.0.1.1\t$1/g" /etc/hosts
+sudo sed -i "s/^.*\$/$1/g" /etc/hostname
